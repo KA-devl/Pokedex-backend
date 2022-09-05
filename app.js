8890a186d0cfcdb241982ebf4fbb99e05c47197b
Anes Khadiri <anes1999@live.ca>
2022-09-05 00:48:48 -0400

    removing morgan module

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require ('express')
-const morgan = require ('morgan') //middleware
 const favicon = require ('serve-favicon')//middleware 2
 const bodyParser = require('body-parser')
 //const cors = require('cors')
@@ -14,7 +13,6 @@ const port = process.env.PORT || 3000
 //Middleware use
 app
 .use(favicon(__dirname +'/favicon.ico'))
-.use(morgan('dev'))
 .use(bodyParser.json())
 //.use(cors())
 
