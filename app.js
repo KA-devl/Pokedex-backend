dcf591b27e14a95534d28e74902aa369edc6069e
Anes Khadiri <anes1999@live.ca>
2022-09-10 16:53:24 -0400

    CORS update v8

--- app.js
+++ app.js
@@ -9,22 +9,18 @@ const sequelize = require ('./src/db/sequelize')
 const app = express()
 const port = process.env.PORT || 3000
 
-const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header("Access-Control-Allow-Credentials", "true");
-  console.log("cors");
-  console.log(req.headers.origin);
-  next();
+const corsOptions ={
+  origin:'*', 
+  methods: 'GET,PUT,POST,DELETE',
+  credentials:true,            //access-control-allow-credentials:true
+  headers: 'Content-type'
 }
-
 
 //Middleware use
 app
 .use(favicon(__dirname +'/favicon.ico'))
 .use(bodyParser.json())
-.use(allowCrossDomain);
+.use(cors(corsOptions))
 
 //Initialize db
 sequelize.initDb()
